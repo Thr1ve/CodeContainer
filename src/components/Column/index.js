ccbcb4e32fcac865dd811f88235b5dcd2d06c793
Thr1ve <fict1on.buhler@gmail.com>
2016-06-11 13:35:53 -0400

    change column to span element and add inline-block for css transitions

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import styles from './styles.css';
 
-const Column = ({ highlighted, char }) =>
-  <div className={highlighted ? styles.highlighted : styles.notHighlighted}>
+const Column = ({ highlighted = false, char }) =>
+  <span className={highlighted ? styles.highlighted : styles.notHighlighted}>
     {char}
-  </div>;
+  </span>;
+
+Column.propTypes = {
+  char: PropTypes.string.isRequired,
+  highlighted: PropTypes.bool
+};
 
 export default Column;
