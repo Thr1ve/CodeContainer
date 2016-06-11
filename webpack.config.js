93468bb58662da55644b3b2352852cca69577577
Thr1ve <fict1on.buhler@gmail.com>
2016-06-11 13:12:35 -0400

    add css-modules

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,15 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js/, exclude: /node_modules/, loader: 'babel-loader'}
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]'
+      }
     ]
   },
   plugins: [HTMLWebpackPluginConfig]
