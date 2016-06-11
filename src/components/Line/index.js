28c31e3a867955bd5327adbfb5b99b4e13d1e67d
Thr1ve <fict1on.buhler@gmail.com>
2016-06-11 13:38:16 -0400

    add line component

---
+++ src/components/Line/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './styles.css';
+
+import Column from '../Column';
+
+const Line = () =>
+  <div className={styles.base}>
+    Line!
+  </div>;
+
+export default Line;
